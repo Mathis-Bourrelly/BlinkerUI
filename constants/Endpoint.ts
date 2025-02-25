ceb125af1b16eee82e131a9e301771f9822d3147
Mathis Bourrelly <mathis.bourrelly13@gmail.com>
2025-02-25 15:48:34 +0100

    fix endpoint

--- constants/Endpoint.ts
+++ constants/Endpoint.ts
@@ -1,5 +1,4 @@
 export const Endpoint = {
-    //url: "https://dev.blinker.eterny.fr"
-    //url: "http://dev.blinker.eterny.fr"
-    url: "http://localhost:3011"
+    url: "https://dev.blinker.eterny.fr"
+    //url: "http://localhost:3011"
 }
